940d542f77c2cdb647288e040f228fc90722c694
xaridev <xaridev@gmail.com>
2023-11-15 18:26:52 -0500

    Add project files.

---
+++ PivotDemoBlazor/Data/ISalesInfoDataProvider.cs
@@ -0,0 +1,13 @@
+﻿namespace PivotDemoBlazor.Data
+{
+    using System.Collections.Generic;
+    using System.Runtime.InteropServices;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public interface ISalesInfoDataProvider
+    {
+        Task<IEnumerable<SaleInfo>> GetSalesAsync(CancellationToken ct = default);
+        Task<IEnumerable<SaleInfo>> GetReducedSalesAsync(CancellationToken ct = default);
+    }
+}
---
+++ PivotDemoBlazor/Data/SaleInfo.cs
@@ -0,0 +1,22 @@
+﻿namespace PivotDemoBlazor.Data
+{
+    using DevExpress.Blazor;
+    using System;
+    using System.Runtime.InteropServices;
+
+    public class SaleInfo
+    {
+        public int OrderId { get; set; }
+        public string Region { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public int Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+    public class DataField
+    {
+        public string NameField { get; set; }
+        public PivotGridFieldArea Area { get; set; }
+    }
+}
